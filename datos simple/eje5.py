e68d7bb436f7eb6b2cfac4b6789d3c58a4d4edab
aymane our <ourhzifaymane@gmail.com>
2026-09-23 12:36:12 +0200

    cambio

---
+++ datos simple/eje5.py
@@ -0,0 +1,3 @@
+horas = int(input("Introduce las horas: "))
+paga = int(input("Introduce la paga por hora: "))
+print(f"La paga total es: {float(horas) * float(paga)}")
---
+++ datos simple/eje6.py
@@ -0,0 +1,3 @@
+num = int(input("Introduce un número: "))
+suma= (num * (num + 1)) / 2
+print("La suma de los números del 1 al", num, "es:", suma)
